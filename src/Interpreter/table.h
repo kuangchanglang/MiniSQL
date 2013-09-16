94b11dd3cbe0d308af81ed278916f2f7b46786f0
backing <kuangchanglang@163.com>
2013-09-16 20:58:02 +0800

    add table.h database.h

---
+++ src/Interpreter/table.h
@@ -0,0 +1,25 @@
+#ifndef __TABLE_H__
+#define __TABLE_H__
+struct attr{
+	string attr_name;
+	int attr_type;
+	bool is_unique;
+};
+
+struct table{
+	string tname;
+	int attr_num;
+	attr attrs[];
+	int primarykey_pos;
+	table(string name, int num, int pos=-1){
+		tname = name;
+		attr_num = num;
+		attrs = new attr[attr_num];
+		primarykey_pos = pos;
+	}
+	~table(){
+		delete []attrs;
+	}
+};
+
+#endif
